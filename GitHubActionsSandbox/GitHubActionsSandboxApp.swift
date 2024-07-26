564faf8895d4232655df8a8bc8356048eeea4dc3
eguchi-ap <t.eguchi@alphapolis.co.jp>
2024-07-26 10:29:31 +0900

    Initial Commit

---
+++ GitHubActionsSandbox/GitHubActionsSandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubActionsSandboxApp.swift
+//  GitHubActionsSandbox
+//
+//  Created by WEBSYSTEM-MAC39 on 2024/07/26.
+//
+
+import SwiftUI
+
+@main
+struct GitHubActionsSandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
