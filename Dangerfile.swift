4f4900b785b1e4848724bec6fd209c2c38d10d92
eguchi-ap <t.eguchi@alphapolis.co.jp>
2024-07-31 14:47:40 +0900

    .swiftlint.yml追加

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,11 +1,4 @@
 import Danger
-import Foundation
-
 let danger = Danger()
 
-// swiftlint
-SwiftLint.lint(inline: true, configFile: ".swiftlint.yml")
-
-if !danger.swiftLintViolations.isEmpty {
-    fail("SwiftLint violations found")
-}
+SwiftLint.lint()
